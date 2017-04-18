ed031b7336db62530b76935ddd241a28e694607e
Craftbox <cbox@MacBook-Pro-de-KOKAINA.local>
2017-04-18 12:34:42 +0100

    fix(javassit): Not Found Exception when adding adapters

--- src/main/java/io/toast/tk/maven/plugin/RunScriptsMojo.java
+++ src/main/java/io/toast/tk/maven/plugin/RunScriptsMojo.java
@@ -2,19 +2,13 @@ package io.toast.tk.maven.plugin;
 
 import com.google.inject.Module;
 import io.toast.tk.adapter.cache.ToastCache;
-import io.toast.tk.core.annotation.Action;
-import io.toast.tk.core.annotation.ActionAdapter;
 import io.toast.tk.dao.domain.impl.test.block.ITestPage;
 import io.toast.tk.maven.plugin.run.TestPageRunner;
 import io.toast.tk.plugin.IAgentPlugin;
 import io.toast.tk.plugin.PluginLoader;
 import io.toast.tk.runtime.parse.FileHelper;
 import io.toast.tk.runtime.parse.TestParser;
-import javassist.ClassPool;
-import javassist.CtClass;
-import javassist.CtMethod;
-import javassist.Modifier;
-import javassist.bytecode.ClassFile;
+
 import org.apache.maven.artifact.DependencyResolutionRequiredException;
 import org.apache.maven.model.FileSet;
 import org.apache.maven.plugin.AbstractMojo;
@@ -27,12 +21,10 @@ import org.apache.maven.plugins.annotations.ResolutionScope;
 import org.apache.maven.project.MavenProject;
 import org.codehaus.plexus.util.FileUtils;
 
-import java.io.DataInputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.lang.reflect.Method;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLClassLoader;
 import java.util.*;
@@ -65,7 +57,7 @@ public class RunScriptsMojo extends AbstractMojo {
             try {
                 extendPluginClassPath();
             } catch (DependencyResolutionRequiredException e) {
-                getLog().error(e);
+                throw new MojoExecutionException(e.getMessage(), e);
             }
 
             initReportOutputDir();
@@ -96,7 +88,7 @@ public class RunScriptsMojo extends AbstractMojo {
         }
     }
 
-    public static List<File> toFileList(FileSet scriptSet) throws IOException {
+    private List<File> toFileList(FileSet scriptSet) throws IOException {
         File directory = new File(scriptSet.getDirectory());
         String includes = toString(scriptSet.getIncludes());
         String excludes = toString(scriptSet.getExcludes());
@@ -138,14 +130,14 @@ public class RunScriptsMojo extends AbstractMojo {
         });
     }
 
-    public ITestPage run(ITestPage testPage) throws IOException {
+    protected ITestPage run(ITestPage testPage) throws IOException {
         getLog().info("Agent plugin class loader: " + IAgentPlugin.class.getClassLoader());
         this.testPageRunner = new TestPageRunner(this.outputDirectory, this.pluginModules);
         return testPageRunner.runTestPage(testPage);
     }
 
-    public static void addURL(URL u) throws IOException {
-        URLClassLoader sysloader = (URLClassLoader) ToastCache.class.getClassLoader();
+    public static void addURL(URL u, ClassLoader classLoader) throws IOException {
+        URLClassLoader sysloader = (URLClassLoader) classLoader;
         Class sysclass = URLClassLoader.class;
         try {
             Method method = sysclass.getDeclaredMethod("addURL", new Class[]{URL.class});
@@ -158,28 +150,15 @@ public class RunScriptsMojo extends AbstractMojo {
     }
 
     private void extendPluginClassPath()
-            throws IOException,
-            DependencyResolutionRequiredException {
+            throws  IOException,
+                    DependencyResolutionRequiredException {
         List<String> elements = project.getRuntimeClasspathElements();
         getLog().info("Extending plugin classpath with project runtime dependencies... " );
+        ClassLoader classLoader = ToastCache.class.getClassLoader();
         for (String element : elements) {
-            addURL(new File(element).toURI().toURL());
-            getLog().info("- " + new File(element).toURI().toURL());
-        }
-
-        getLog().info("Adding adapters from project classpath... ");
-        ClassPool cp= ClassPool.getDefault();
-        File directory = new File(project.getBuild().getOutputDirectory());
-        Iterator<File> iterateFiles = org.apache.commons.io.FileUtils.iterateFiles(directory, new String[]{"class"}, true);
-        while (iterateFiles.hasNext()) {
             try {
-                DataInputStream dataInputStream = new DataInputStream(new FileInputStream(iterateFiles.next()));
-                ClassFile classFile = new ClassFile(dataInputStream);
-                CtClass c = cp.get(classFile.getName());
-                if (c.hasAnnotation(ActionAdapter.class) && !Modifier.isAbstract(c.getModifiers())) {
-                    getLog().info("- " + c.getName());
-                    ToastCache.getInstance().addActionAdapter(c.toClass());
-                }
+                addURL(new File(element).toURI().toURL(), classLoader);
+                getLog().info("- " + new File(element).toURI().toURL());
             } catch (Exception e) {
                 getLog().error(e);
             }
